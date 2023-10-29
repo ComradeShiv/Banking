591ede5323c464d7da5dfb0a5bddddd40e4772d5
= <=>
2023-10-29 10:49:39 +0530

    Attempt 4

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -44,7 +44,7 @@ public class BankAccount {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-        if(sum > 9*digits) throw new AccountNumberCanNotBeGenerated("Account Number can not be generated");
+        if(sum < digits || sum > 9*digits) throw new AccountNumberCanNotBeGenerated("Account Number can not be generated");
 
         Random random = new Random();
         ArrayList<String> numberArray = new ArrayList<>();
@@ -52,7 +52,7 @@ public class BankAccount {
         for(int i = 0; i < digits-1; i++) {
             int possibleDigit = random.nextInt(10);
 
-            while(sum - possibleDigit > 9 * (digits - i - 1))
+            while(sum - possibleDigit < digits - i - 1 || sum - possibleDigit > 9 * (digits - i - 1))
                 possibleDigit = random.nextInt(10);
 
             sum -= possibleDigit;
